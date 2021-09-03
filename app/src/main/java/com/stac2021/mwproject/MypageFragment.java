0cd68ca2b0e59fcdbf27e25ad1315eea984f6d0f
Hajin Kim <rhrnak624@naver.com>
2021-09-04 03:07:35 +0900

    Changed mypage recent cardview into Dynamic

--- app/src/main/java/com/stac2021/mwproject/MypageFragment.java
+++ app/src/main/java/com/stac2021/mwproject/MypageFragment.java
@@ -9,19 +9,28 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MypageFragment extends Fragment {
 
+    RecyclerView recyclerView;
+    MypageCardViewAdapter adapter;
+    String[] itemTitle = {"생리대 사이즈 종류", "쓰레기 분리수거 하는법", "세탁기 돌리는 방법", "전구 갈아끼우는 방법", "생리 용퓸 종류들을 알려줄게!", "피임약 복용 방법을 알려줄게!",
+            "월경 주기 계산 방법을 알려줄게!", "생리대 사용 방법을 알려줄게!", "생리컵 사용 방법을 알려줄게"};
+    Integer[] itemImage = {R.drawable.thumbnail1, R.drawable.thumbnail01, R.drawable.thumbnail2, R.drawable.thumbnail02, R.drawable.thumbnail3, R.drawable.thumbnail03, R.drawable.thumbnail4, R.drawable.thumbnail04, R.drawable.thumbnail5};
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_mypage, container, false);
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+        adapter = new MypageCardViewAdapter(itemImage, itemTitle);
+        recyclerView.setAdapter(adapter);
 
 
-
-
-        출처: https://okoos.tistory.com/entry/안드로이드-RecyclerView-사용하기-예제 [쏙's]
         return view;
     }
 }
