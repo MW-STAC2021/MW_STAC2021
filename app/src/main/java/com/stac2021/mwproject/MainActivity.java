cb30d44c0beeddcbed0a79f6d7953b25a13c5a94
LEE MINJI <m04j00@gmail.com>
2021-09-19 02:06:23 +0900

    사용하지 않는 import 정리

--- app/src/main/java/com/stac2021/mwproject/MainActivity.java
+++ app/src/main/java/com/stac2021/mwproject/MainActivity.java
@@ -1,20 +1,10 @@
 package com.stac2021.mwproject;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MenuItem;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.tabs.TabLayout;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import androidx.viewpager.widget.ViewPager;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
@@ -22,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         getSupportFragmentManager().beginTransaction().add(R.id.frameLayout, new HomeFragment()).commit();
         bottomNavigationView = findViewById(R.id.navigationView);
         // FrameLayout에 fragment.xml 띄우기
